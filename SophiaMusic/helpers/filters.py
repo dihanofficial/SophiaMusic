4be9d3f3fe86474215e55e89d5f474b61079b88b
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-08-13 14:45:01 +0530

    Update filters.py

--- SophiaMusic/helpers/filters.py
+++ SophiaMusic/helpers/filters.py
@@ -2,7 +2,7 @@ from typing import List, Union
 
 from pyrogram import filters
 
-from DaisyXMusic.config import COMMAND_PREFIXES
+from SophiaMusic.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
