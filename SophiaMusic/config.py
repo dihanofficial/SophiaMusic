5bb471dc9047ce9e3d01292f5fce36a72fffaaf5
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-08-13 15:31:06 +0530

    SophiaMusic

---
+++ SophiaMusic/config.py
@@ -0,0 +1,30 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DihanOfficial")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/3654bea3e5284b442f932.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "NA")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "dihan_official")
+PROJECT_NAME = getenv("PROJECT_NAME", "Sophia Music v6")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/dihanofficial/SophiaMusic")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+PMMSG = getenv("PMMSG", f"Hi there, This is a music assistant service of @{BOT_USERNAME}.\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don t add this user to secret groups.\n   - Don t Share private info here\n\n")
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
