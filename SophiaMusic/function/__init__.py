3bb3a73824f398c8a6de9d17bb85b6124a825c49
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-08-13 16:57:16 +0530

    Update __init__.py

--- SophiaMusic/function/__init__.py
+++ SophiaMusic/function/__init__.py
@@ -3,4 +3,4 @@ from SophiaMusic.function.admins import admins
 from SophiaMusic.function.admins import get
 from SophiaMusic.function.admins import set
 
-__all__ = ["set", "get", "admins"
+__all__ = ["set", "get", "admins"]
