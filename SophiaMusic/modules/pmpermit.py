f05190b07cd504f4707f4af75922be453468acae
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-08-13 14:54:23 +0530

    SophiaMusic

---
+++ SophiaMusic/modules/pmpermit.py
@@ -0,0 +1,75 @@
+
+
+""" Sophia Music v6 """
+
+
+
+from pyrogram import Client
+import asyncio
+from SophiaMusic.config import SUDO_USERS, PMPERMIT, PMMSG
+from pyrogram import filters
+from pyrogram.types import Message
+from SophiaMusic.services.callsmusic.callsmusic import client as USER
+
+PMSET =True
+pchats = []
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+    if PMPERMIT == "ENABLE":
+        if PMSET:
+            chat_id = message.chat.id
+            if chat_id in pchats:
+                return
+            try:
+              await USER.send_message(
+                  message.chat.id,
+                  PMMSG
+              )
+              return
+            except:
+              return
+    
+
+@Client.on_message(filters.command(["/pmpermit"]))
+async def bye(client: Client, message: Message):
+    if message.from_user.id in SUDO_USERS:
+        global PMSET
+        text = message.text.split(" ", 1)
+        queryy = text[1]
+        if queryy == "on":
+            PMSET = True
+            await message.reply_text("Pmpermit turned on")
+            return
+        if queryy == "off":
+            PMSET = None
+            await message.reply_text("Pmpermit turned off")
+            return
+
+@USER.on_message(filters.text & filters.private & filters.me)        
+async def autopmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM due to outgoing messages")
+        return
+    message.continue_propagation()    
+    
+@USER.on_message(filters.command("a", [".", ""]) & filters.me & filters.private)
+async def pmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM")
+        return
+    message.continue_propagation()    
+    
+
+@USER.on_message(filters.command("da", [".", ""]) & filters.me & filters.private)
+async def rmpmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if chat_id in pchats:
+        pchats.remove(chat_id)
+        await message.reply_text("Dispprooved to PM")
+        return
+    message.continue_propagation()    
