42d762acb4d5390ec4e50189750a5b547aa78932
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-08-13 14:32:12 +0530

    Sophia

--- SophiaMusic/__main__.py
+++ SophiaMusic/__main__.py
@@ -1,5 +1,5 @@
-# EzilaXMusic (Telegram bot project)
-# Copyright (C) 2021  Sadew Jayasekara & Rojserbest
+# SophiaMusic (Telegram bot project)
+# Copyright (C) 2021  Dihan Official
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
